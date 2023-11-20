a4c93bb02b781daf4c81fa85874656f81391ce20
Malekqandeel <malikqan@gmail.com>
2023-11-20 23:55:28 +0300

    Layout and function

---
+++ main.js
@@ -0,0 +1,22 @@
+const product = [
+    {
+        id:1 ,
+        title: " Product 1",
+        imageSrc : "",
+        description :"",
+        amount : 22,
+        category: ""
+    },
+    {
+        id:2 ,
+        title: " Product 1",
+        imageSrc : "",
+        description :"",
+        amount : 22,
+        category: ""
+    },
+
+
+
+
+]
